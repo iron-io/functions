8023f15d1a6979315acd8c831f7521f39f654799
Henrique Chehad <hchehad@gmail.com>
2016-08-29 20:01:06 -0300

    removed env payload in ruby hello example

--- examples/hello/hello.rb
+++ examples/hello/hello.rb
@@ -4,14 +4,6 @@ name = "World"
 
 payload = STDIN.read
 if payload != ""
-    puts "Received from STDIN: #{payload}"
-end
-
-# or using env vars: ENV['PAYLOAD']
-payload = ENV['PAYLOAD']
-
-STDERR.puts 'payload: ' + payload.inspect
-if payload != ""
     payload = JSON.parse(payload)
     name = payload['name']
 end
