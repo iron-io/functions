69948e605bbed7154326e9d0412a66beb78795cd
Pedro Nasser <pedronasser@gmail.com>
2016-10-03 14:37:02 -0300

    Fix runner dependency

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-export GO15VENDOREXPERIMENT=1
+export GO15VENDOREXPERIMENT=1		
 export LOG_LEVEL=debug
 export IGNORE_MEMORY=1
-
-go test -v $(go list ./... | grep -v /vendor/ | grep -v /examples/)
+  		  
+ go test -v $(go list ./... | grep -v /vendor/ | grep -v /examples/) 
