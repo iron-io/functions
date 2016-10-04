31f50874433b26e52e4817fe7f7975b08fdaf169
Pedro Nasser <pedronasser@gmail.com>
2016-10-04 16:59:21 -0300

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
-export GO15VENDOREXPERIMENT=1
 export LOG_LEVEL=debug
 export IGNORE_MEMORY=1
 
-go test -v $(go list ./... | grep -v /vendor/ | grep -v /examples/)
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock \
+    -e IGNORE_MEMORY=1 \
+    -e LOG_LEVEL=debug \
+    -e GOPATH="$PWD/../../../.." \
+    -v "$PWD":"$PWD" -w "$PWD" iron/go-dind go test -v $(go list ./... | grep -v /vendor/ | grep -v /examples/)
