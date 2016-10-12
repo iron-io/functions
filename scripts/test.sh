16b1cde5e00d1e58b4980bb5addd266df8bebc35
Pedro Nasser <pedronasser@gmail.com>
2016-10-12 15:57:48 -0300

    try test without docker

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -ti --privileged --rm -e GIN_MODE=$GIN_MODE -e LOG_LEVEL=debug -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD":/go/src/github.com/iron-io/functions -w /go/src/github.com/iron-io/functions iron/go:dev go test -v $(glide nv | grep -v examples | grep -v tool)
+export LOG_LEVEL=debug
+go test -v $(glide nv | grep -v examples | grep -v tool)
