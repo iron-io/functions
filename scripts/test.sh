3e38445044c41b45aca20fd4dbc30f128563de96
Pedro Nasser <pedronasser@gmail.com>
2016-10-12 16:55:05 -0300

    reverse, add docker to test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-export LOG_LEVEL=debug
-go test -v $(glide nv | grep -v examples | grep -v tool)
+docker run -ti --privileged --rm -e GIN_MODE=$GIN_MODE -e LOG_LEVEL=debug -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD":/go/src/github.com/iron-io/functions -w /go/src/github.com/iron-io/functions iron/go:dev go test -v $(glide nv | grep -v examples | grep -v tool)
