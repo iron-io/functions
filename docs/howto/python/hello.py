6a4a3fe8d171d2c8187e5cfbfce7d155d6dc45ca
Carlos C <uldericofilho@gmail.com>
2016-11-01 15:53:15 -0700

    docs: fix Python example
    Used an idiomatic method (isatty) to decide whether stdin must
    be read or not.

--- docs/howto/python/hello.py
+++ docs/howto/python/hello.py
@@ -1,24 +1,12 @@
 import sys
 sys.path.append("packages")
-import fcntl
 import os
 import json
 
-def non_block_read(output):
-    fd = output.fileno()
-    fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-    fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-    try:
-        return output.read()
-    except:
-        return ""
-
-try:
-	content = non_block_read(sys.stdin)
-	obj = json.loads(content)
+name = "World"
+if not os.isatty(sys.stdin.fileno()):
+	obj = json.loads(sys.stdin.read())
 	if obj["name"] != "":
 		name = obj["name"]
-except ValueError:
-	name = "World"
 
 print "Hello", name, "!!!"
