bebf4c5583c4fa906a32ec60186aff6069f52828
Carlos C <uldericofilho@gmail.com>
2016-11-11 19:24:44 +0100

    fnctl: conciliate init with master
    Fixes #266

--- fnctl/init.go
+++ fnctl/init.go
@@ -11,23 +11,57 @@ import (
 	"github.com/urfave/cli"
 )
 
+var (
+	fileExtToRuntime = map[string]string{
+		".c":     "gcc",
+		".class": "java",
+		".clj":   "leiningen",
+		".cpp":   "gcc",
+		".erl":   "erlang",
+		".ex":    "elixir",
+		".go":    "go",
+		".h":     "gcc",
+		".java":  "java",
+		".js":    "node",
+		".php":   "php",
+		".pl":    "perl",
+		".py":    "python",
+		".scala": "scala",
+	}
+
+	fnRuntimes []string
+)
+
+func init() {
+	for rt := range acceptableFnRuntimes {
+		fnRuntimes = append(fnRuntimes, rt)
+	}
+}
+
 type initFnCmd struct {
-	force bool
+	force   bool
+	runtime string
 }
 
 func initFn() cli.Command {
 	a := initFnCmd{}
 
 	return cli.Command{
-		Name:      "init",
-		Usage:     "create a local function.yaml file",
-		ArgsUsage: "fnctl init <entrypoint>",
-		Action:    a.init,
+		Name:        "init",
+		Usage:       "create a local function.yaml file",
+		Description: "Entrypoint is the binary file which the container engine will invoke when the request comes in - equivalent to Dockerfile ENTRYPOINT.",
+		ArgsUsage:   "<entrypoint>",
+		Action:      a.init,
 		Flags: []cli.Flag{
 			cli.BoolFlag{
 				Name:        "f",
 				Usage:       "overwrite existing function.yaml",
 				Destination: &a.force,
+			},
+			cli.StringFlag{
+				Name:        "runtime",
+				Usage:       "choose an existing runtime - " + strings.Join(fnRuntimes, ", "),
+				Destination: &a.runtime,
 			},
 		},
 	}
@@ -44,17 +78,52 @@ func (a *initFnCmd) init(c *cli.Context) error {
 
 	entrypoint := c.Args().First()
 	if entrypoint == "" {
+		fmt.Print("Please, specify an entrypoint for your function: ")
+		fmt.Scanln(&entrypoint)
+	}
+	if entrypoint == "" {
 		return errors.New("entrypoint is missing")
 	}
 
-	scores := map[string]uint{
-		"": 0,
-	}
 	pwd, err := os.Getwd()
 	if err != nil {
 		return fmt.Errorf("error detecting current working directory: %s\n", err)
 	}
-	err = filepath.Walk(pwd, func(_ string, info os.FileInfo, _ error) error {
+
+	if a.runtime == "" {
+		rt, err := detectRuntime(pwd)
+		if err != nil {
+			return err
+		}
+		var ok bool
+		a.runtime, ok = fileExtToRuntime[rt]
+		if !ok {
+			return fmt.Errorf("could not detect language of this function: %s\n", a.runtime)
+		}
+	}
+
+	if _, ok := acceptableFnRuntimes[a.runtime]; !ok {
+		return fmt.Errorf("cannot use runtime %s", a.runtime)
+	}
+
+	ff := &funcfile{
+		Runtime:    &a.runtime,
+		Version:    initialVersion,
+		Entrypoint: &entrypoint,
+	}
+
+	if err := encodeFuncfileYAML("function.yaml", ff); err != nil {
+		return err
+	}
+	fmt.Println("function.yaml written")
+	return nil
+}
+
+func detectRuntime(path string) (string, error) {
+	scores := map[string]uint{
+		"": 0,
+	}
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, _ error) error {
 		if info.IsDir() {
 			return nil
 		}
@@ -67,7 +136,7 @@ func (a *initFnCmd) init(c *cli.Context) error {
 		return nil
 	})
 	if err != nil {
-		return fmt.Errorf("file walk error: %s\n", err)
+		return "", fmt.Errorf("file walk error: %s\n", err)
 	}
 
 	biggest := ""
@@ -77,34 +146,6 @@ func (a *initFnCmd) init(c *cli.Context) error {
 		}
 	}
 
-	runtime, ok := fileExtToRuntime[biggest]
-	if !ok {
-		return fmt.Errorf("could not detect language of this function: %s\n", biggest)
-	}
-	ff := &funcfile{
-		Image: runtime,
-		// Runtime:   runtime,
-		Version: initialVersion,
-		// Entrypoint: c.Args().First(),
-	}
+	return biggest, nil
 
-	encodeFuncfileYAML("function.yaml", ff)
-	return nil
-}
-
-var fileExtToRuntime = map[string]string{
-	".c":     "gcc",
-	".class": "java",
-	".clj":   "leiningen",
-	".cpp":   "gcc",
-	".erl":   "erlang",
-	".ex":    "elixir",
-	".go":    "go",
-	".h":     "gcc",
-	".java":  "java",
-	".js":    "node",
-	".php":   "php",
-	".pl":    "perl",
-	".py":    "python",
-	".scala": "scala",
 }
