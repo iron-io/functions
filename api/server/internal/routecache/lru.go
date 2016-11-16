a7f34e9fef6af54cc837b6921db96dbebaf15e06
Carlos C <uldericofilho@gmail.com>
2016-11-16 19:51:34 +0100

    functions: improve LRU internals

--- api/server/internal/routecache/lru.go
+++ api/server/internal/routecache/lru.go
@@ -14,7 +14,7 @@ import (
 const defaultMaxEntries = 100
 
 // Cache holds an internal linkedlist for hotness management. It is not safe
-// for concurrent use, must be guarded externally. Its zero value means nothing.
+// for concurrent use, must be guarded externally.
 type Cache struct {
 	maxentries int
 
@@ -39,23 +39,33 @@ func New() *Cache {
 // Routes is an ordered slice with the hottest routes at the beginning, so to
 // increase the likelihood of matching with incoming requests.
 func (c *Cache) Routes() []*models.Route {
+	if c.cache == nil {
+		return []*models.Route{}
+	}
+
 	return c.values
 }
 
 // Refresh updates internal linkedlist either adding a new route to the front,
 // or moving it to the front when used. It will discard seldom used routes.
 func (c *Cache) Refresh(route *models.Route) {
+	if c.cache == nil {
+		c.maxentries = defaultMaxEntries
+		c.ll = list.New()
+		c.cache = make(map[*models.Route]*list.Element)
+	}
+
 	if ee, ok := c.cache[route]; ok {
 		c.ll.MoveToFront(ee)
 		ee.Value.(*routecacheentry).route = route
 		c.updatevalues()
 		return
 	}
+
 	ele := c.ll.PushFront(&routecacheentry{route})
 	c.cache[route] = ele
 	if c.maxentries != 0 && c.ll.Len() > c.maxentries {
 		c.removeOldest()
-		return
 	}
 
 	c.updatevalues()
@@ -66,15 +76,6 @@ func (c *Cache) updatevalues() {
 	for e := c.ll.Front(); e != nil; e = e.Next() {
 		route := e.Value.(*routecacheentry).route
 		c.values = append(c.values, route)
-	}
-}
-
-func (c *Cache) remove(key *models.Route) {
-	if c.cache == nil {
-		return
-	}
-	if ele, hit := c.cache[key]; hit {
-		c.removeElement(ele)
 	}
 }
 
@@ -92,5 +93,4 @@ func (c *Cache) removeElement(e *list.Element) {
 	c.ll.Remove(e)
 	kv := e.Value.(*routecacheentry)
 	delete(c.cache, kv.route)
-	c.updatevalues()
 }
