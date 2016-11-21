ddd9d419be10452f337e367c0d7853e94dea1464
Carlos C <uldericofilho@gmail.com>
2016-11-21 22:01:52 +0100

    fn: fix docker incantation

--- fn/langs/dotnet.go
+++ fn/langs/dotnet.go
@@ -6,11 +6,10 @@ import (
 	"os/exec"
 )
 
-type DotNetLangHelper struct {
-}
+type DotNetLangHelper struct{}
 
 func (lh *DotNetLangHelper) Entrypoint() string {
-	return "dotnet func.dll"
+	return "dotnet dotnet.dll"
 }
 
 func (lh *DotNetLangHelper) HasPreBuild() bool {
@@ -23,9 +22,13 @@ func (lh *DotNetLangHelper) PreBuild() error {
 	if err != nil {
 		return err
 	}
-	// todo: this won't work if the function is more complex since the import paths won't match up, need to fix
-	cmd := exec.Command("docker", "run", "--rm", "-v", wd+":/dotnet", "-w", "/dotnet", "microsoft/dotnet", "dotnet restore")
-	// cmd.Dir = dir
+
+	cmd := exec.Command(
+		"docker", "run",
+		"--rm", "-v",
+		wd+":/dotnet", "-w", "/dotnet", "microsoft/dotnet",
+		"/bin/sh", "-c", "dotnet restore && dotnet publish -c release -b /tmp -o .",
+	)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
