2863092f178a11a79e1189803ce96053331195a1
Seif Lotfy <seif.lotfy@gmail.com>
2016-11-23 23:11:11 +0100

    update comment on rust.go

--- fn/langs/rust.go
+++ fn/langs/rust.go
@@ -16,7 +16,7 @@ func (lh *RustLangHelper) HasPreBuild() bool {
 	return true
 }
 
-// PreBuild for Go builds the binary so the final image can be as small as possible
+// PreBuild for rust builds the binary so the final image can be as small as possible
 func (lh *RustLangHelper) PreBuild() error {
 	wd, err := os.Getwd()
 	if err != nil {
