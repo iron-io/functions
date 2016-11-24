1fe5a6c20a51d71700f4a88934f729a8d823285e
Pedro Nasser <pedronasser@gmail.com>
2016-11-24 15:12:38 -0200

    fix bug when filter is nil

--- api/datastore/bolt/bolt.go
+++ api/datastore/bolt/bolt.go
@@ -518,7 +518,7 @@ func (ds *BoltDatastore) Get(ctx context.Context, key []byte) ([]byte, error) {
 }
 
 func applyAppFilter(app *models.App, filter *models.AppFilter) bool {
-	if filter.Name != "" {
+	if filter != nil && filter.Name != "" {
 		nameLike, err := regexp.MatchString(strings.Replace(filter.Name, "%", ".*", -1), app.Name)
 		return err == nil && nameLike
 	}
