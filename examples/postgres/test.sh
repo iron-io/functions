526e170180a2db8581c647623fc4a0c3f038112e
Jordan Krage <jmank88@gmail.com>
2017-01-26 18:24:21 -0600

    test.sh cleanup

--- examples/postgres/test.sh
+++ examples/postgres/test.sh
@@ -14,26 +14,26 @@ RECORD1='{
     "last": "Smith",
     "age": 30
 }'
-echo $RECORD1 | /home/jordan/go/bin/fn run -e SERVER=postgres:5432 -e COMMAND=INSERT -e TABLE=people --link test-postgres-func:postgres
+echo $RECORD1 | fn run -e SERVER=postgres:5432 -e COMMAND=INSERT -e TABLE=people --link test-postgres-func:postgres
 QUERY1='{
     "last": "Smith"
 }'
-echo $QUERY1 | /home/jordan/go/bin/fn run -e SERVER=postgres:5432 -e COMMAND=SELECT -e TABLE=people --link test-postgres-func:postgres
+echo $QUERY1 | fn run -e SERVER=postgres:5432 -e COMMAND=SELECT -e TABLE=people --link test-postgres-func:postgres
 
 RECORD2='{
     "first": "Bob",
     "last": "Smith",
     "age": 43
 }'
-echo $RECORD2 | /home/jordan/go/bin/fn run -e SERVER=postgres:5432 -e COMMAND=INSERT -e TABLE=people --link test-postgres-func:postgres
+echo $RECORD2 | fn run -e SERVER=postgres:5432 -e COMMAND=INSERT -e TABLE=people --link test-postgres-func:postgres
 
-echo $QUERY1 | /home/jordan/go/bin/fn run -e SERVER=postgres:5432 -e COMMAND=SELECT -e TABLE=people --link test-postgres-func:postgres
+echo $QUERY1 | fn run -e SERVER=postgres:5432 -e COMMAND=SELECT -e TABLE=people --link test-postgres-func:postgres
 
 QUERY2='{
     "first": "John",
     "last": "Smith"
 }'
-echo $QUERY2 | /home/jordan/go/bin/fn run -e SERVER=postgres:5432 -e COMMAND=SELECT -e TABLE=people --link test-postgres-func:postgres
+echo $QUERY2 | fn run -e SERVER=postgres:5432 -e COMMAND=SELECT -e TABLE=people --link test-postgres-func:postgres
 
 docker stop test-postgres-func
 docker rm test-postgres-func
