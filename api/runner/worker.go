cce8f79209629af94ad4b9c397cf0959a2256cd4
Pedro Nasser <pedronasser@gmail.com>
2017-03-21 20:57:28 -0300

    add env vars for hot functions

--- api/runner/worker.go
+++ api/runner/worker.go
@@ -296,6 +296,8 @@ func (hc *htfn) serve(ctx context.Context) {
 	}()
 
 	cfg := *hc.cfg
+	cfg.Env["FN_HOT"] = "true"
+	cfg.Env["FN_FORMAT"] = cfg.Format
 	cfg.Timeout = 0 // add a timeout to simulate ab.end. failure.
 	cfg.Stdin = hc.containerIn
 	cfg.Stdout = hc.containerOut
