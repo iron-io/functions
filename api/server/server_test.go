8396e662c6910cf579b6293cc8e11c6341cf8d54
Arda Karaduman <akaraduman@gmail.com>
2018-01-15 23:59:30 +0900

    cleanup test files after test

--- api/server/server_test.go
+++ api/server/server_test.go
@@ -114,7 +114,6 @@ func getErrorResponse(t *testing.T, rec *httptest.ResponseRecorder) models.Error
 }
 
 func prepareBolt(t *testing.T) models.Datastore {
-	os.Remove(tmpBolt)
 	ds, err := datastore.New("bolt://" + tmpBolt)
 	if err != nil {
 		t.Fatal("Error when creating datastore: %s", err)
